64a60ccfaa7b427e0d4128fe37d156ae02902de0
Olufemi Oluoje <kobomarun@gmail.com>
2016-09-20 14:19:37 +0100

    BMP-29: Sidebar: Pull Latest products from DB

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -22,62 +22,28 @@
   </div>
   <div class="bmp-aside-latest">
     <div class="post-header">
-      Latest
+      Latest Products
     </div>
+    <?php
+      $this->db->order_by("name", "asc");
+      $this->db->limit(4);
+      $query =  $this->db->get('bmp_products')->result();
+    ?>
+    <?php foreach($query as $product) { ?>
     <div class="row ">
       <a href="#">
       <div class="col-sm-5 ">
-        <img src="<?php echo base_url(); ?>img/blocks.jpeg" class="img-responsive"/>
+        <img src="<?php echo base_url(); ?>img/<?php echo $product->img; ?>" class="img-responsive"/>
       </div>
       <div class="col-sm-7 text">
         <div class="bmp-prod-cat">Sub-category name</div>
-        <div class="bmp-prod-name">Product name</div>
-        <div class="bmp-prod-price">₦1200</div>
+        <div class="bmp-prod-name"><?php echo $product->name; ?></div>
+        <div class="bmp-prod-price">Guide Price: ₦<?php echo $product->price; ?></div>
       </div>
       </a>
     </div>
     <div class="divider"></div>
-
-    <div class="row">
-      <a href="#">
-      <div class="col-sm-5 ">
-       <img src="<?php echo base_url(); ?>img/iron.jpeg" class="img-responsive"/>
-      </div>
-      <div class="col-sm-7 text">
-        <div class="bmp-prod-cat">Sub-category name</div>
-        <div class="bmp-prod-name">Product name</div>
-        <div class="bmp-prod-price">₦1200</div>
-      </div>
-      </a>
-    </div>
-    <div class="divider"></div>
-
-    <div class="row">
-      <a href="#">
-      <div class="col-sm-5 ">
-       <img src="<?php echo base_url(); ?>img/pan.jpeg" class="img-responsive"/>
-      </div>
-      <div class="col-sm-7 text">
-        <div class="bmp-prod-cat">Sub-category name</div>
-        <div class="bmp-prod-name">Product name</div>
-        <div class="bmp-prod-price">₦1200</div>
-      </div>
-      </a>
-    </div>
-    <div class="divider"></div>
-
-    <div class="row">
-      <a href="#">
-      <div class="col-sm-5 ">
-        <img src="<?php echo base_url(); ?>img/branite.jpeg" class="img-responsive"/>
-      </div>
-      <div class="col-sm-7 text">
-        <div class="bmp-prod-cat">Sub-category name</div>
-        <div class="bmp-prod-name">Product name</div>
-        <div class="bmp-prod-price">₦1200</div>
-      </div>
-      </a>
-    </div>
+    <?php } ?>
   </div>
 </aside>
 </div>
